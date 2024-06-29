36ef15b96fc4f38d1c18703515dd42de98b47c87
alanliongar <129612933+alanliongar@users.noreply.github.com>
2024-06-29 09:58:38 -0300

    Até fim da aula 9 de room database - 2

--- app/src/main/java/com/devspace/taskbeats/MainActivity.kt
+++ app/src/main/java/com/devspace/taskbeats/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.devspace.taskbeats
-//Onde parei? Terminei aula 9, ver aula 10 agora.
+//Onde parei? Terminei aula 9, ver aula 10 agora. -
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings.Global
